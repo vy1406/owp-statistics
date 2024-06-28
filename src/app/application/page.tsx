b3c484f2df6bcb41944db80fad626fc262f8347f
Vladimir Elisavetski <yvova88@yahoo.com>
2024-06-28 14:31:37 -0600

    wip

--- src/app/application/page.tsx
+++ src/app/application/page.tsx
@@ -1,5 +1,4 @@
 import { db } from '@/db';
-import ApplicationCreateForm from '@/components/create-application-form';
 import CollapsibleApplicationBox from '@/components/common/collapse-card';
 
 export const dynamic = 'force-dynamic'
@@ -29,7 +28,6 @@ export default async function ApplicationsPage() {
 
     return (
         <div>
-            <ApplicationCreateForm />
             <div className="flex flex-col gap-2">{renderApplications}</div>
         </div>
     );
