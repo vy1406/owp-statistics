37e298db24fce639b91ea9ecaf3a9d5a56b58e1a
Vladimir Elisavetski <yvova88@yahoo.com>
2024-07-02 20:42:57 -0600

    integrating google analytics

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,11 +1,9 @@
 import React, { Suspense } from "react"
 import Signin from "./sign-in"
-import GoogleAalytics from "@/components/google_analytics"
 
 const Page = () => {
   return (
     <Suspense>
-      <GoogleAalytics />
       <Signin />
     </Suspense>
   )
